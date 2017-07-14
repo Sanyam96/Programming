ccc86b50c0e12c9478fb7715fefb1eeb4e51b381
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-07-14 23:07:50 +0530

    GCD of Two Numbers

---
+++ Basics1/GCDofTwoNumbers.cpp
@@ -0,0 +1,50 @@
+// GCD of Two Numbers
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	int n1, n2;
+
+    cout << "Enter two numbers: ";
+    cin >> n1 >> n2;
+    
+    while(n1 != n2)
+    {
+        if(n1 > n2)
+            n1 -= n2;
+        else
+            n2 -= n1;
+    }
+
+    cout << "HCF = " << n1 << endl;
+    return 0;
+}
+
+
+/*
+int gcd(int a, int b)
+{
+    // Everything divides 0 
+    if (a == 0 || b == 0)
+       return 0;
+ 
+    // base case
+    if (a == b)
+        return a;
+ 
+    // a is greater
+    if (a > b)
+        return gcd(a-b, b);
+    return gcd(a, b-a);
+}
+ 
+// Driver program to test above function
+int main()
+{
+    int a = 98, b = 56;
+    printf("GCD of %d and %d is %d ", a, b, gcd(a, b));
+    return 0;
+}
+*/
