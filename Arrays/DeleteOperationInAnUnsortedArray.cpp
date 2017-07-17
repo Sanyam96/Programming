89d9de22eb19d1ff8b1b76cb9f2ffca8d1e752ae
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-07-17 22:08:36 +0530

    Delete Operation in an unsorted Array

---
+++ Arrays/DeleteOperationInAnUnsortedArray.cpp
@@ -0,0 +1,52 @@
+// Delete Operation In An Unsorted Array
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int findElement(int arr[], int n, int key)
+{
+    int i;
+    for (i=0; i<n; i++)
+        if (arr[i] == key)
+            return i;
+ 
+    return -1;
+}
+
+int deleteElement(int arr[], int n, int key)
+{
+    int pos = findElement(arr, n, key);
+ 
+    if (pos==-1)
+    {
+        printf("Element not found");
+        return n;
+    }
+ 
+    int i;
+    for (i=pos; i<n-1; i++)
+        arr[i] = arr[i+1];
+ 
+    return n-1;
+}
+ 
+int main()
+{
+    int i;
+    int arr[] = {10, 50, 30, 40, 20};
+ 
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int key = 30;
+ 
+    printf("Array before deletion\n");
+    for (i=0; i<n; i++)
+      printf("%d  ", arr[i]);
+ 
+    n = deleteElement(arr, n, key);
+ 
+    printf("\n\nArray after deletion\n");
+    for (i=0; i<n; i++)
+      printf("%d  ", arr[i]);
+ 
+ 	printf("\n");
+    return 0;
+}
