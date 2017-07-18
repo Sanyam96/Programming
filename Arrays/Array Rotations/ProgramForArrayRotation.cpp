96332da6ba4cf7ae61b9d7c5a45249a4df8cfc86
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-07-18 23:01:07 +0530

    Array Rotation approach 2

--- Arrays/Array Rotations/ProgramForArrayRotation.cpp
+++ Arrays/Array Rotations/ProgramForArrayRotation.cpp
@@ -9,6 +9,41 @@ Output : [3, 4, 5, 6, 7, 1, 2]
 #include <bits/stdc++.h>
 using namespace std;
 
+// Approach 2
+// Rotate one by one ==>> Time complexity: O(n*d) , Auxiliary Space: O(1)
+int main() {
+
+	int n = 7;
+	int d = 2;
+	int arr[] = {1, 2, 3, 4, 5, 6, 7};
+	int tempVariable;
+
+	int temp = arr[0];
+	for (int i = 0; i < d; ++i)
+	{
+		int tempVariable = arr[0];
+		for (int j = 0; j < n-1; ++j)
+		{
+			arr[j] = arr[j+1];
+		}
+		arr[n-1] = tempVariable;
+	}
+	cout << "Now Array is : ";
+	for (int i = 0; i < n; ++i)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << "\n";
+
+	return 0;
+}
+
+
+
+
+
+/*
+// Approach 1
 // By temp Array ==>>  Time complexity O(n) , Auxiliary Space: O(d)
 int main() {
 
@@ -19,22 +54,19 @@ int main() {
 
 	for (int i = 0; i < d; ++i)
 	{
-		/* code */
 		temp[i] = arr[i];
 	}
 
 	int j = 0;
 	for (int i = d; i < n; ++i)
 	{
-		/* code */
 		arr[j] = arr[i];
 		j++;
 	}
-	
+
 	int k = 0;
 	for (int i = j; i < n; ++i)
 	{
-		/* code */
 		arr[i] = temp[k];
 		k++;
 	}
@@ -42,11 +74,9 @@ int main() {
 	cout << "Now Array is : ";
 	for (int i = 0; i < n; ++i)
 	{
-		/* code */
 		cout << arr[i] << " ";
 	}
 	cout << "\n";
-
-
     return 0;
 }
+*/
