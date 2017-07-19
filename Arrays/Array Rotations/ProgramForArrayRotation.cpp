d24cd5c4bbc601e4f84bbab7419a30515a83dd87
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-07-19 22:54:21 +0530

    Array Rotation approach 5

--- Arrays/Array Rotations/ProgramForArrayRotation.cpp
+++ Arrays/Array Rotations/ProgramForArrayRotation.cpp
@@ -9,6 +9,66 @@ function rotate(ar[], d, n) that rotates arr[] of size n by d elements.
 #include <bits/stdc++.h>
 using namespace std;
 
+
+// Approach 5
+// Block Swap Implementation ==>> Recursive approach  :: Time Complexity: O(n)
+// Reference Link: http://www.geeksforgeeks.org/block-swap-algorithm-for-array-rotation/
+void printArray(int arr[], int size);
+void swap(int arr[], int fi, int si, int d);
+ 
+void leftRotate(int arr[], int d, int n)
+{  
+  if(d == 0 || d == n)
+    return;
+     
+  if( n-d == d)
+  {
+    swap(arr, 0, n-d, d);   
+    return;
+  }  
+
+  if(d < n-d)
+  {  
+    swap(arr, 0, n-d, d);
+    leftRotate(arr, d, n-d);    
+  }    
+  else             
+  {
+    swap(arr, 0, d, n-d);
+    leftRotate(arr+n-d, 2*d-n, d); // This is tricky
+  }
+}
+ 
+void printArray(int arr[], int size)
+{
+  int i;
+	for(i = 0; i < size; i++)
+		printf("%d ", arr[i]);
+	
+	printf("\n ");
+} 
+
+void swap(int arr[], int fi, int si, int d)
+{
+   int i, temp;
+   for( i = 0; i < d; i++)   
+   {
+     temp = arr[fi + i];
+     arr[fi + i] = arr[si + i];
+     arr[si + i] = temp;
+   }     
+}     
+ 
+int main()
+{
+   int arr[] = {1, 2, 3, 4, 5, 6, 7};
+   leftRotate(arr, 2, 7);
+   printArray(arr, 7);
+   return 0;
+}
+
+
+/*
 // Approach 4
 // The Reversal Algorithm ==>> Time Complexity: O(n)
 // Reference Link: http://www.geeksforgeeks.org/program-for-array-rotation-continued-reversal-algorithm/
@@ -54,6 +114,7 @@ int main()
     cout << endl;
     return 0;
 }
+*/
 
 /*
 // Approach 3
