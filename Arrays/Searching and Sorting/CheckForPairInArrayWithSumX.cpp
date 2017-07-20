6dcb165d8a5df885439b24a60243a63c87635059
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-07-21 03:20:03 +0530

    Given an array A[] and a number x, check for pair in A[] with sum as x Approach2

--- Arrays/Searching and Sorting/CheckForPairInArrayWithSumX.cpp
+++ Arrays/Searching and Sorting/CheckForPairInArrayWithSumX.cpp
@@ -3,10 +3,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Approach 2
+// Using Hash Map
+// This method works in O(n) time if range of numbers is known.
+#define MAX 100000
+ 
+void printPairs(int arr[], int arr_size, int sum)
+{
+  int i, temp;
+  bool binMap[MAX] = {0}; // initialize hash map as 0
+ 
+  for (i = 0; i < arr_size; i++)
+  {
+      temp = sum - arr[i];
+      if (temp >= 0 && binMap[temp] == 1)
+         printf("Pair with given sum %d is (%d, %d) n", 
+                 sum, arr[i], temp);
+      binMap[arr[i]] = 1;
+  }
+}
+ 
+int main()
+{
+    int A[] = {1, 4, 45, 6, 10, 8};
+    int n = 16;
+    int arr_size = sizeof(A)/sizeof(A[0]);
+ 
+    printPairs(A, arr_size, n);
+ 
+    getchar();
+    return 0;
+}
+
+
+
+
+/*
+
 // Approach 1
 // Using Sorting
 // Time Space : If we use Merge Sort or Heap Sort then (-)(nlogn) in worst case. If we use Quick Sort then O(n^2) in worst case.
 // Auxiliary Space : Again, depends on sorting algorithm. For example auxiliary space is O(n) for merge sort and O(1) for Heap Sort.
+# define bool int
 
 void quickSort(int *, int, int);
  
@@ -14,11 +52,10 @@ bool hasArrayTwoCandidates(int A[], int arr_size, int sum)
 {
     int l, r;
  
-    /* Sort the elements */
+    // Sort the elements 
     quickSort(A, 0, arr_size-1);
  
-    /* Now look for the two candidates in the sorted 
-       array*/
+    // Now look for the two candidates in the sorted array
     l = 0;
     r = arr_size-1; 
     while (l < r)
@@ -33,7 +70,6 @@ bool hasArrayTwoCandidates(int A[], int arr_size, int sum)
     return 0;
 }
  
-/* Driver program to test above function */
 int main()
 {
     int A[] = {1, 4, 45, 6, 10, -8};
@@ -49,8 +85,6 @@ int main()
     return 0;
 }
  
-/* FOLLOWING FUNCTIONS ARE ONLY FOR SORTING 
-    PURPOSE */
 void exchange(int *a, int *b)
 {
     int temp;
@@ -77,14 +111,14 @@ int partition(int A[], int si, int ei)
     return (i + 1);
 }
  
-/* Implementation of Quick Sort
-A[] --> Array to be sorted
-si  --> Starting index
-ei  --> Ending index
-*/
+//  Implementation of Quick Sort
+// A[] --> Array to be sorted
+// si  --> Starting index
+// ei  --> Ending index
+
 void quickSort(int A[], int si, int ei)
 {
-    int pi;    /* Partitioning index */
+    int pi;    // Partitioning index
     if(si < ei)
     {
         pi = partition(A, si, ei);
@@ -92,3 +126,4 @@ void quickSort(int A[], int si, int ei)
         quickSort(A, pi + 1, ei);
     }
 }
+*/
