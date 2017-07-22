2e46490b46db325f0e6ef32138f0579f3768fbff
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-07-22 21:48:52 +0530

    Reverse of an Array Recursive method approach

--- Arrays/Arrangement Rearrangement/ReverseOfArray.cpp
+++ Arrays/Arrangement Rearrangement/ReverseOfArray.cpp
@@ -9,6 +9,41 @@ output : 5 4 3 2 1
 #include <bits/stdc++.h>
 using namespace std;
 
+// Approach 2
+// Recursive Way
+// Time Complexity: O(n)
+
+void reverseArray(int arr[], int start, int end) {
+
+	int temp;
+	if (start >= end) {
+ 		return;
+	}
+	temp = arr[start];   
+	arr[start] = arr[end];
+	arr[end] = temp;
+	reverseArray(arr, start+1, end-1);   
+}     
+ 
+void printArray(int arr[], int size) {
+	
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
+}
+
+int main(int argc, char const *argv[]) {
+	int arr[] = {1, 2, 3, 4, 5};
+    printArray(arr, 5);
+    reverseArray(arr, 0, 4);
+    cout << "Reverse Array is n : ";
+    printArray(arr, 5);    
+	return 0;
+}
+
+/*
 // Approach 1
 // Iterative Method
 // Time Complexity: O(n)
@@ -45,3 +80,4 @@ int main() {
 
     return 0;
 }
+*/
