350fe30e9b119eaca113382a8a05eaefa08edbc6
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-07-22 15:01:17 +0530

    Majority Element Approach 3

--- Arrays/Searching and Sorting/MajorityElement.cpp
+++ Arrays/Searching and Sorting/MajorityElement.cpp
@@ -15,6 +15,84 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Approach 3
+// using Array Of Array
+
+int maxElement(int arr[], int size) {
+	int max = arr[0];
+	for (int i = 1; i < size; ++i) {
+		if ( max <= arr[i] ) {
+			max = arr[i];
+		}	
+	}
+	return max;
+}
+
+int findCandidate(int arr[], int size) {
+
+	int sizeOfTempArray = maxElement(arr, size);
+	int *tempArray = new int[sizeOfTempArray];
+
+	for (int i = 0; i <= sizeOfTempArray+1; ++i) {
+		tempArray[i] = 0;
+	}
+
+	for (int i = 0; i <= size; ++i) {
+		tempArray[arr[i]] = tempArray[arr[i]] + 1;
+	}
+
+	int maxElementOccuring = maxElement(tempArray, sizeOfTempArray+1);
+	int indexOfMaxElement = 0;
+	for (int i = 0; i < sizeOfTempArray+1; ++i)	{
+		if(tempArray[i] == maxElementOccuring) {
+			indexOfMaxElement = i;
+		}
+	}
+	delete [] tempArray;
+	return indexOfMaxElement;
+}
+
+bool isMajority(int a[], int size, int cand)
+{
+	int i, count = 0;
+
+	for (i = 0; i < size; i++)
+		if (a[i] == cand) {
+			count++;
+		}
+		if (count > size/2) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+}
+
+void printMajority(int a[], int size)
+{
+	int cand = findCandidate(a, size);
+
+	if (isMajority(a, size, cand)) {
+		printf("%d ", cand);
+	}
+	else {
+		printf("No Majority Element");
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	int a[] = {3, 3, 4, 2, 4, 4, 2, 4, 4};
+    int size = (sizeof(a))/sizeof(a[0]);
+    printMajority(a, size);
+    // int x = findCandidate(a, size);
+    // cout << "\n" << x << endl;
+    cout << endl;
+	return 0;
+}
+
+
+/*
 // Approach 2
 // Using Moore’s Voting Algorithm
 // Time Complexity: O(n)
@@ -78,6 +156,7 @@ int main(int argc, char const *argv[]) {
 
 	return 0;
 }
+*/
 
 /*
 // Approach 1
