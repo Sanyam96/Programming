541ad5702ee701bb22abe0271b3a532d93344cac
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-07-27 01:40:52 +0530

    Print Hello World Without Semicolon in C++ approach3 by using Macros

--- Random/PrintHelloWorldWithoutSemicolon.cpp
+++ Random/PrintHelloWorldWithoutSemicolon.cpp
@@ -1,5 +1,21 @@
 // Print Hello World Without Semicolon in C++ 
 
+// Method 3 by Using macros
+#include <bits/stdc++.h>
+using namespace std;
+#define GEEK printf("Hello World\n")
+
+int main() {
+
+	if(GEEK) {
+
+	}
+	
+    return 0;
+}
+
+
+/*
 // Method 2 Using switch statement
 #include <bits/stdc++.h>
 using namespace std;
@@ -11,7 +27,7 @@ int main() {
 	}
     return 0;
 }
-
+*/
 
 /*
 // Method 1 by If condition!
