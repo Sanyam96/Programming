7e737892830bc54a25c1e7a79b3d39a420c7d9ee
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-07-27 23:24:43 +0530

    Question1- Conflict between Local and Global variable

---
+++ Input and Output Questions/Declarations and Initializations/Question1.cpp
@@ -0,0 +1,22 @@
+// What will be the output of the following program
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int x = 40;
+
+int main() {
+
+	int x = 20;
+	printf("%d\n", x );
+    return 0;
+}
+
+
+
+// -----------------------------------------------------------------
+/*
+	Output: 20
+	// In case of conflict between a local variable and global variable
+	// Local variable gets a priority.
+*/
