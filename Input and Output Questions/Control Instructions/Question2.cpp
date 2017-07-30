82b570a5a8aa7f1be9b25ed05170875a8b2c609d
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-07-31 01:43:14 +0530

    Switch Statement2

---
+++ Input and Output Questions/Control Instructions/Question2.cpp
@@ -0,0 +1,27 @@
+// What is the output of the following  program and also Point out the Error If Any
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	int a = 10;
+	switch(a) {
+
+	}
+	cout << "Hello Sanyam" << endl;
+    return 0;
+}
+
+
+// ----------------------------------------------------------------------------------
+/*
+
+	Output :
+		Hello Sanyam
+
+	Reason :
+		There can be a switch, which has no cases!!
+		Therefore no error in the program
+
+*/
