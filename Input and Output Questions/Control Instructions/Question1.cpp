b5a509d2fdba9049c90595bdf981d2a79c8ea078
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-07-31 01:39:54 +0530

    Basic Switch Statement

---
+++ Input and Output Questions/Control Instructions/Question1.cpp
@@ -0,0 +1,38 @@
+// What is the output of the following  program
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	int i = 4;
+	switch(i) {
+		
+		default : 
+			cout << "In Default" << endl;
+
+		case 1 :
+			cout << "Case1" << endl;
+			break;
+
+		case 2 :
+			cout << "Case2" << endl;
+			break;
+
+		case 3 : 
+			cout << "Case3" << endl;
+			break;
+	}
+    return 0;
+}
+
+
+// ----------------------------------------------------------------------------------
+/*
+
+	Output : 
+		In Default
+		Case1
+
+*/
+
