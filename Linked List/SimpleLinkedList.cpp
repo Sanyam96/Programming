04399b6aa75492f736c5f60a3288325f77a51569
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-10-04 19:16:09 +0530

    Modified Linked List

--- Linked List/SimpleLinkedList.cpp
+++ Linked List/SimpleLinkedList.cpp
@@ -3,6 +3,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Node {
+public:
+	int data;
+	Node * next;
+};
+
+void print( Node * head) {
+	while( head != NULL) {
+		cout << head -> data << " --> ";
+		head = head -> next;
+	}
+}
+
+int main(int argc, char const *argv[]) {
+	
+	Node * head = NULL;
+	Node * second = NULL;
+	Node * third = NULL;	
+
+	head = new Node;
+	second = new Node;
+	third = new Node;
+
+	head -> data = 1;
+	head -> next = second;
+
+	second -> data = 2;
+	second -> next = third;
+
+	third -> data = 3;
+	third -> next = NULL;
+
+	print(head);
+	cout << "\n";
+
+	return 0;
+}
+
+/*
 struct Node {
 
 	int data;
@@ -33,3 +72,4 @@ int main() {
 
     return 0;
 }
+*/
