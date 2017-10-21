65f7e54a2b6617c6f10cf8234fb31227704eee3e
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-10-21 21:41:44 +0530

    Arrangment And Rearrangement Solution Updated

--- Arrays/Arrangement Rearrangement/RearrangePositiveAndNegativeNumbers.cpp
+++ Arrays/Arrangement Rearrangement/RearrangePositiveAndNegativeNumbers.cpp
@@ -14,15 +14,16 @@ Output should be [9, -7, 8, -3, 5, -1, 2, 4, 6]
 #include <bits/stdc++.h>
 using namespace std;
 
-void swap(int *a, int *b);
- 
-void rearrange( int arr[], int n)
-{
+void swap( int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void rearrange( int arr[], int n) {
     int i = -1;
-    for( int j = 0; j < n; j++)
-    {
-        if (arr[j] < 0)
-        {
+    for( int j = 0; j < n; j++) {
+        if (arr[j] < 0) {
             i++;
             swap(&arr[i], &arr[j]);
         }
@@ -30,29 +31,19 @@ void rearrange( int arr[], int n)
  
     int pos = i+1, neg = 0;
  
-    while( pos < n && neg < pos && arr[neg] < 0)
-    {
+    while( pos < n && neg < pos && arr[neg] < 0) {
         swap( &arr[neg], &arr[pos]);
         pos++;
         neg += 2;
     }
 }
  
-void swap( int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
- 
-void printArray(int arr[], int n)
-{
+void printArray(int arr[], int n) {
     for (int i = 0; i < n; i++)
         printf("%4d ", arr[i]);
 }
  
-int main()
-{
+int main(int argc, char const *argv[]) {
     int arr[] = {-1, 2, -3, 4, 5, 6, -7, 8, 9};
     int n = sizeof(arr)/sizeof(arr[0]);
     rearrange(arr, n);
